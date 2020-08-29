5437ee35246de80922bbb56cf0499a91424f4e04
pratik <pratiksherke97@gmail.com>
2020-08-29 20:04:05 +0530

    refactoring app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,22 @@
-window.addEventListener("load", () => {
-  const apiKey = config.WEATHER_API_KEY;
+const apiKey = config.WEATHER_API_KEY;
 
-  let long;
-  let lat;
-  const temperatureDescription = document.querySelector(
-    ".temperature-description"
-  );
-  const temperatureDegree = document.querySelector(".temperature-degree");
-  const locationTimezone = document.querySelector(".location-timezone");
-  const location = document.querySelector(".location");
-  const temperatureSection = document.querySelector(".temperature-section");
-  const temperatureSectionSpan = document.querySelector(".scale");
+let long;
+let lat;
+const temperatureDescription = document.querySelector(
+  ".temperature-description"
+);
+const temperatureDegree = document.querySelector(".temperature-degree");
+const locationTimezone = document.querySelector(".location-timezone");
+const temperatureSection = document.querySelector(".temperature-section");
+const temperatureSectionSpan = document.querySelector(".scale");
 
+function display(event) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
+      const location = document.querySelector(".location");
       const api = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&units=imperial&appid=${apiKey}`;
 
       fetch(api)
@@ -37,27 +37,33 @@ window.addEventListener("load", () => {
           location.appendChild(setIconImage(icon));
 
           //change temp scales.
-          temperatureSection.addEventListener("click", () => {
-            if (temperatureSectionSpan.textContent === "F") {
-              temperatureSectionSpan.textContent = "C";
-              temperatureDegree.textContent = ((temp - 32) * (5 / 9)).toFixed(
-                1
-              );
-            } else {
-              temperatureSectionSpan.textContent = "F";
-              temperatureDegree.textContent = temp;
-            }
-          });
+          temperatureSection.addEventListener("click", (tempClickEvent) =>
+            changeTemperatureScale(tempClickEvent, temp)
+          );
         });
     });
   }
+}
 
-  function setIconImage(icon) {
-    const iconApi = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+function setIconImage(icon) {
+  const iconApi = `http://openweathermap.org/img/wn/${icon}@4x.png`;
 
-    let img = document.createElement("img");
-    img.src = iconApi;
+  let img = document.createElement("img");
+  img.src = iconApi;
 
-    return img;
+  return img;
+}
+
+function changeTemperatureScale(tempClickEvent, temp) {
+  {
+    if (temperatureSectionSpan.textContent === "F") {
+      temperatureSectionSpan.textContent = "C";
+      temperatureDegree.textContent = ((temp - 32) * (5 / 9)).toFixed(1);
+    } else {
+      temperatureSectionSpan.textContent = "F";
+      temperatureDegree.textContent = temp;
+    }
   }
-});
+}
+
+window.addEventListener("load", display);
