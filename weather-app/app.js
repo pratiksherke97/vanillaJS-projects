4bf60ca653af200450f27b51f56b728818b2b472
pratik <pratiksherke97@gmail.com>
2020-08-30 23:23:29 +0530

    adding an analog clock to the app

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,6 +9,27 @@ const temperatureDegree = document.querySelector(".temperature-degree");
 const locationTimezone = document.querySelector(".location-timezone");
 const temperatureSection = document.querySelector(".temperature-section");
 const temperatureSectionSpan = document.querySelector(".scale");
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
 
 function display(event) {
   if (navigator.geolocation) {
@@ -24,7 +45,6 @@ function display(event) {
           return response.json();
         })
         .then((data) => {
-          console.log(data);
           const { temp } = data.current;
           const { description, icon } = data.current.weather[0];
 
